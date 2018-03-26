6391987f301bbad4c66e188427c070bc179c99d1
kumar <kiranreddyamk@gmail.com>
2018-03-25 21:43:44 -0500

    Never stops

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -3,8 +3,8 @@ package org.pictolearn.docker;
 public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 100; i++){
-			System.out.println("Kiran Hi World Ping " + i );
+		for(int i = 1 ; true; i++){
+			System.out.println("Kiran Ping  - Never stops" + i );
 			Thread.sleep(1000);
 		}
 	}
